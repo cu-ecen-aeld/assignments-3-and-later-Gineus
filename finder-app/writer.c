827cb37a9315121e01bcb0ee8d00ec321ab455de
Gineus <41481916+Gineus@users.noreply.github.com>
2025-02-25 23:27:22 +0000

    Fix: Write writestr to writefile and handle possible errors

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -3,6 +3,7 @@
 #include <syslog.h>
 #include <string.h>
 
+
 int main(int argc, char *argv[])
 {
     if (argc != 3)
@@ -15,14 +16,25 @@ int main(int argc, char *argv[])
     char *writefile = argv[1];
     char *writestr = argv[2];
     syslog(LOG_DEBUG, "Writing %s to %s", writestr, writefile);
-    closelog();
 
-    if (errno != 0)
+    FILE *file = fopen(writefile, "w");
+    if (file == NULL)
     {
-        fprintf(stderr, "Error: %s\n", strerror(errno));
-        perror("Error");
+        syslog(LOG_ERR, "Failed to open file %s: %s", writefile, strerror(errno));
+        closelog();
         return 1;
     }
+
+    if (fprintf(file, "%s", writestr) < 0)
+    {
+        syslog(LOG_ERR, "Failed to write to file %s: %s", writefile, strerror(errno));
+        fclose(file);
+        closelog();
+        return 1;
+    }
+
+    fclose(file);
+    closelog();
 
     return 0;
 }
