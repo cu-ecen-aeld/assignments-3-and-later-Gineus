48b1fa6ba80b44aa5a59afb1fb20e77e8a2d2105
Gineus <41481916+Gineus@users.noreply.github.com>
2025-03-06 17:06:12 +0000

    Install busybox in the right directory

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -90,7 +90,9 @@ fi
 
 # Make and install busybox
 make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX="${OUTDIR}/rootfs" install
+
+cd "${OUTDIR}/rootfs"
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
