dfe16d7ff87afc9de573f13c0807e3b8f0372be1
Gineus <41481916+Gineus@users.noreply.github.com>
2025-03-06 18:35:05 +0000

    Copied autorun-qemu.sh to home directory

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -127,6 +127,7 @@ cp "${FINDER_APP_DIR}/dependencies.sh" "${OUTDIR}/rootfs/home"
 cp "${FINDER_APP_DIR}/writer" "${OUTDIR}/rootfs/home"
 cp "${FINDER_APP_DIR}/../conf/username.txt" "${OUTDIR}/rootfs/home"
 cp "${FINDER_APP_DIR}/../conf/assignment.txt" "${OUTDIR}/rootfs/home"
+cp "${FINDER_APP_DIR}/autorun-qemu.sh" "${OUTDIR}/rootfs/home"
 
 # Chown the root directory
 sudo chown -R root:root ${OUTDIR}/rootfs
