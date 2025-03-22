6ba44758a31cad5909bbda98615b4637c43e679c
Gineus <41481916+Gineus@users.noreply.github.com>
2025-03-22 15:36:34 +0000

    Write the output of the finder command to /tmp/assignment4-result.txt

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -59,6 +59,9 @@ done
 
 OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
+# Write the output to /tmp/assignment4-result.txt
+echo "${OUTPUTSTRING}" > /tmp/assignment4-result.txt
+
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
